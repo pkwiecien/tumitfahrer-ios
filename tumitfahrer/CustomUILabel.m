6a963ea5394c1763193fe92aeb7d7d37ba41ab9c
Pawel Kwiecien <p.kwiecien@tum.de>
2014-05-06 16:07:28 +0200

    add new custom ui label, improve your rides by showing a respecive label when there are no rides

---
+++ tumitfahrer/CustomUILabel.m
@@ -0,0 +1,31 @@
+//
+//  CustomUILabel.m
+//  tumitfahrer
+//
+//  Created by Pawel Kwiecien on 5/6/14.
+//  Copyright (c) 2014 Pawel Kwiecien. All rights reserved.
+//
+
+#import "CustomUILabel.h"
+
+@implementation CustomUILabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
