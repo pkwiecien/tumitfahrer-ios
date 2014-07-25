e434b289ea89692355462d6d25c5b05919531aea
Pawel Kwiecien <p.kwiecien@tum.de>
2014-07-25 10:21:05 +0200

    change program name and package needed for deployment

--- XcodeCoverage/env.sh
+++ XcodeCoverage/env.sh
@@ -1,5 +1,5 @@
-export BUILT_PRODUCTS_DIR="/Users/pawel/Library/Developer/Xcode/DerivedData/tumitfahrer-fbahuqrhvawhpqfljxjkkrxdtjwh/Build/Products/Release-iphoneos"
+export BUILT_PRODUCTS_DIR="/Users/pawel/Library/Developer/Xcode/DerivedData/tumitfahrer-fbahuqrhvawhpqfljxjkkrxdtjwh/Build/Intermediates/ArchiveIntermediates/tumitfahrer/BuildProductsPath/Release-iphoneos"
 export CURRENT_ARCH="arm64"
-export OBJECT_FILE_DIR_normal="/Users/pawel/Library/Developer/Xcode/DerivedData/tumitfahrer-fbahuqrhvawhpqfljxjkkrxdtjwh/Build/Intermediates/tumitfahrer.build/Release-iphoneos/tumitfahrer.build/Objects-normal"
-export OBJROOT="/Users/pawel/Library/Developer/Xcode/DerivedData/tumitfahrer-fbahuqrhvawhpqfljxjkkrxdtjwh/Build/Intermediates"
+export OBJECT_FILE_DIR_normal="/Users/pawel/Library/Developer/Xcode/DerivedData/tumitfahrer-fbahuqrhvawhpqfljxjkkrxdtjwh/Build/Intermediates/ArchiveIntermediates/tumitfahrer/IntermediateBuildFilesPath/tumitfahrer.build/Release-iphoneos/mitfahrer-app.build/Objects-normal"
+export OBJROOT="/Users/pawel/Library/Developer/Xcode/DerivedData/tumitfahrer-fbahuqrhvawhpqfljxjkkrxdtjwh/Build/Intermediates/ArchiveIntermediates/tumitfahrer/IntermediateBuildFilesPath"
 export SRCROOT="/Users/pawel/Documents/dev/Objective-C/tumitfahrer"
